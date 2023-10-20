6102df14a588bda698dfbd33759d16909bfcee5c
Tracy Qiu <qiu.tr@northeastern.edu>
2023-10-20 10:32:52 -0400

    fix leaderboard linting

--- ips/ip2-handout/frontend/src/components/Town/interactables/Leaderboard.tsx
+++ ips/ip2-handout/frontend/src/components/Town/interactables/Leaderboard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import { GameResult } from '../../../types/CoveyTownSocket';
 
@@ -21,6 +22,7 @@ export default function Leaderboard({ results }: { results: GameResult[] }): JSX
     const { scores } = result;
     const playerNames = Object.keys(scores);
 
+    // add names to list of unique names
     playerNames.forEach(name => {
       if (!playerResults[name]) {
         playerResults[name] = { wins: 0, losses: 0, ties: 0 };
@@ -33,7 +35,7 @@ export default function Leaderboard({ results }: { results: GameResult[] }): JSX
     if (playerOneScore == 1 && playerTwoScore == 0) {
       playerResults[playerOneName].wins++;
       playerResults[playerTwoName].losses++;
-    } else if (playerOneScore == 0 && playerTwoScore == 0) {
+    } else if (playerOneScore == 1 && playerTwoScore == 1) {
       playerResults[playerOneName].ties++;
       playerResults[playerTwoName].ties++;
     } else if (playerTwoScore == 1 && playerOneScore == 0) {
@@ -42,38 +44,23 @@ export default function Leaderboard({ results }: { results: GameResult[] }): JSX
     }
   });
 
+  // convert list of objects to an array and sort them
   const sortedPlayerStatsArray = Object.entries(playerResults).sort(
     (a, b) => b[1].wins - a[1].wins,
   );
 
-  const sortedPlayerStats = sortedPlayerStatsArray.reduce(
-    (acc, [key, value]) => {
-      acc[key] = value;
-      return acc;
-    },
-    {} as { [name: string]: { wins: number; losses: number; ties: number } },
+  // convert back to objects
+  // eslint-disable-next-line prettier/prettier
+  const sortedPlayerStats = sortedPlayerStatsArray.reduce((acc, [key, value]) => {
+    // eslint-disable-next-line prettier/prettier
+    acc[key] = value;
+    // eslint-disable-next-line prettier/prettier
+    return acc;
+    // eslint-disable-next-line prettier/prettier
+    }, {} as { [name: string]: { wins: number; losses: number; ties: number } },
   );
 
-  // playerResults.sort((a, b) => b.wins - a.wins);
-
-  // const statsArray = Object.entries(playerResults).map(([name, stats]) => ({
-  //   name,
-  //   wins: stats.wins,
-  //   losses: stats.losses,
-  //   ties: stats.ties,
-  // }));
-
-  // statsArray.sort((a, b) => b.wins - a.wins);
-
-  // const sortedPlayerStats = statsArray.reduce((acc, player) => {
-  //   acc[player.name] = {
-  //     wins: player.wins,
-  //     losses: player.losses,
-  //     ties: player.ties,
-  //   };
-  //   return acc;
-  // }, {});
-
+  // return leaderboard table
   return (
     <table>
       <thead>
